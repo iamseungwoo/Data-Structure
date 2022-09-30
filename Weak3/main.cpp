a998636e454194189f243debd81b803ffc843923
seungwoo <mongoose@likelion.org>
2022-10-01 02:14:41 +0900

    somebody help me

--- Weak3/main.cpp
+++ Weak3/main.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,65 +8,58 @@ struct polynomial {
     float coefficient; // 계수
 };
 
-int termCntArr[] = { 0, 0, 0 };
+int termCntArr[] = {0, 0, 0};
 
-void draw(polynomial* P, int n) {
+void draw(polynomial *P, int n) {
     for (int i = 0; i < n; i++) {
         printf("%8.3f %8d\n", P[i].coefficient, P[i].exponenet);
     }
 }
 
-int cmp(const void* A, const void* B) {
-    if (((polynomial*)A)->exponenet < ((polynomial*)B)->exponenet) {
+int cmp(const void *A, const void *B) {
+    if (((polynomial *)A)->exponenet < ((polynomial *)B)->exponenet) {
         return 1;
-    }
-    else if (((polynomial*)A)->exponenet > ((polynomial*)B)->exponenet) {
+    } else if (((polynomial *)A)->exponenet > ((polynomial *)B)->exponenet) {
         return -1;
-    }
-    else {
+    } else {
         return 0;
     }
 }
 
-
-polynomial* padd(polynomial* A, polynomial* B) {
-    polynomial* temp = (polynomial*)malloc(sizeof(polynomial) * (termCntArr[0] + termCntArr[1]));
+polynomial *padd(polynomial *A, polynomial *B) {
+    polynomial *temp = (polynomial *)malloc(sizeof(polynomial) * (termCntArr[0] + termCntArr[1]));
     int aTerm = 0;
     int bTerm = 0;
     int tempTerm = 0;
+
     while (1) {
-        if (aTerm == termCntArr[0] && bTerm == termCntArr[1]) {
+        if (aTerm >= termCntArr[0] && bTerm >= termCntArr[1]) {
             break;
         }
         if (A[aTerm].exponenet > B[bTerm].exponenet) {
             temp[tempTerm].exponenet = A[aTerm].exponenet;
             temp[tempTerm++].coefficient = A[aTerm++].coefficient;
-
-
-        }
-        else if (A[aTerm].exponenet < B[bTerm].exponenet) {
+            continue;
+        } else if (A[aTerm].exponenet < B[bTerm].exponenet) {
             temp[tempTerm].exponenet = B[bTerm].exponenet;
             temp[tempTerm++].coefficient = B[bTerm++].coefficient;
-
-
-        }
-        else if (A[aTerm].exponenet == B[bTerm].exponenet) {
+            continue;
+        } else if (A[aTerm].exponenet == B[bTerm].exponenet) {
             temp[tempTerm].exponenet = A[aTerm].exponenet;
             temp[tempTerm++].coefficient = A[aTerm++].coefficient + B[bTerm++].coefficient;
-
+            continue;
         }
     }
     termCntArr[2] = tempTerm;
-    temp = (polynomial*)realloc(temp, sizeof(polynomial) * tempTerm);
     return temp;
 }
 
-polynomial* pmul(polynomial* A, polynomial* B) {
+polynomial *pmul(polynomial *A, polynomial *B) {
     int cycle = termCntArr[0];
     termCntArr[0] = 0;
-    polynomial* D = (polynomial*)malloc(sizeof(polynomial)*0);
+    polynomial *D = (polynomial *)malloc(sizeof(polynomial) * 0);
     for (int i = 0; i < cycle; i++) {
-        polynomial* Ci = (polynomial*)malloc(sizeof(polynomial) * termCntArr[1]);
+        polynomial *Ci = (polynomial *)malloc(sizeof(polynomial) * termCntArr[1]);
         for (int j = 0; j < termCntArr[1]; j++) {
             Ci[j].exponenet = A[i].exponenet + B[j].exponenet;
             Ci[j].coefficient = A[i].coefficient * B[j].coefficient;
@@ -73,16 +67,19 @@ polynomial* pmul(polynomial* A, polynomial* B) {
         printf("C%d(x)\n", i + 1);
         draw(Ci, termCntArr[1]);
         D = padd(D, Ci);
+        D = (polynomial *)realloc(D, (sizeof(polynomial) * termCntArr[2]));
         termCntArr[0] = termCntArr[2];
+        draw(D, termCntArr[0]);
+        free(Ci);
     }
+
     return D;
 }
 
-
 int main() {
-    int initSize[] = { 10, 10 };
-    polynomial* A = (polynomial*)malloc(sizeof(polynomial) * initSize[0]);
-    polynomial* B = (polynomial*)malloc(sizeof(polynomial) * initSize[1]);
+    int initSize[] = {10, 10};
+    polynomial *A = (polynomial *)malloc(sizeof(polynomial) * initSize[0]);
+    polynomial *B = (polynomial *)malloc(sizeof(polynomial) * initSize[1]);
 
     while (1) {
 
@@ -96,14 +93,14 @@ int main() {
         }
 
         if (termCntArr[0] == 9) {
-            A = (polynomial*)realloc(A, sizeof(polynomial) * initSize[0] * 2);
+            A = (polynomial *)realloc(A, sizeof(polynomial) * initSize[0] * 2);
             initSize[0] *= 2;
         }
         A[termCntArr[0]].exponenet = tempExpo;
         A[termCntArr[0]].coefficient = tempCoef;
         termCntArr[0] += 1;
     }
-    qsort((polynomial*)A, termCntArr[0], sizeof(polynomial), cmp);
+    qsort((polynomial *)A, termCntArr[0], sizeof(polynomial), cmp);
 
     printf("다항식 A(x)\n");
     printf("    coef    expon\n");
@@ -123,14 +120,14 @@ int main() {
         }
 
         if (termCntArr[1] == 9) {
-            B = (polynomial*)realloc(B, sizeof(polynomial) * initSize[1] * 2);
+            B = (polynomial *)realloc(B, sizeof(polynomial) * initSize[1] * 2);
             initSize[1] *= 2;
         }
         B[termCntArr[1]].exponenet = tempExpo;
         B[termCntArr[1]].coefficient = tempCoef;
         termCntArr[1] += 1;
     }
-    qsort((polynomial*)B, termCntArr[1], sizeof(polynomial), cmp);
+    qsort((polynomial *)B, termCntArr[1], sizeof(polynomial), cmp);
 
     printf("다항식 B(x)\n");
     printf("    coef    expon\n");
@@ -138,15 +135,13 @@ int main() {
         printf("%8.3f %8d\n", B[i].coefficient, B[i].exponenet);
     }
 
-    polynomial* D = padd(A, B);
-
+    polynomial *D;
+    D = padd(A, B);
     printf("3.2 다항식 덧셈\n다항식 덧셈 결과 : D(x)\n");
     printf("    coef    expon\n");
-    for (int i = 0; i < termCntArr[2]; i++) {
-        printf("%8.3f %8d\n", D[i].coefficient, D[i].exponenet);
-    }
+    draw(D, termCntArr[2]);
 
     D = pmul(A, B);
     printf("D(x) 곱셈\n");
-    draw(D, termCntArr[0]);
+    draw(D, termCntArr[2]);
 }
